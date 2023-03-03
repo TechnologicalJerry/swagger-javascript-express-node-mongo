89c5c14335919e08ae13f4ababf377bb79f13b64
technologicaljerry <cserajatverma@gmail.com>
2023-03-03 21:20:22 +0530

    setup server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Welcome to nodeJs API');
+})
+
+app.listen(port, () => {
+    console.log(`NodeJs Server running on PORT: ${port}`);
+})
