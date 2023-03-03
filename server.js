1776d6270285b112135009c8bd283d71586225fb
technologicaljerry <cserajatverma@gmail.com>
2023-03-03 21:35:13 +0530

    connect database

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongoDb = require('mongoose');
 const app = express();
 const port = 3000;
+const dBhost = 'mongodb://localhost:27017';
 
 app.get('/', (req, res) => {
     res.send('Hello, Welcome to nodeJs API');
 })
+
+app.get('/blog', (req, res) => {
+    res.send('This is blog api');
+})
+
+mongoDb.connect(dBhost).then(() => console.log('MongoDb Connected'));
 
 app.listen(port, () => {
     console.log(`NodeJs Server running on PORT: ${port}`);
