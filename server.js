e88f177ac4267dcbc0267fcb08ed49d2717a57e8
technologicaljerry <cserajatverma@gmail.com>
2023-05-13 16:35:26 +0530

    update

--- server.js
+++ server.js
@@ -21,15 +21,25 @@ database.once('connected', () => {
     console.log('Database Connected');
 })
 
+app.use(express.json());
+
 app.listen(port, () => {
     console.log(`NodeJs Server running on PORT: ${port}`);
 })
 
-const users = ['jerry', 'bill', 'mark'];
+const users = [];
 
 app.get('/users', (req, res) => {
     console.log('Hello, Welcome to nodeJs API!');
     console.log('API Health is OKAY!');
     res.status(200).send(users);
+})
+
+app.post('/users', (req, res) => {
+    let postData = req.body
+    console.log('POSTDATA::', postData);
+    const user = { name: req.body.name, password: req.body.password };
+    users.push(user);
+    res.status(201).send(users);
 })
 
