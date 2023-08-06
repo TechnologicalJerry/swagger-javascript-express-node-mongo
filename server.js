4c893316f5365b110aca68f267168e816b229d4d
technologicaljerry <cserajatverma@gmail.com>
2023-08-06 19:03:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,34 +29,34 @@ app.listen(port, () => {
     console.log(`NodeJs Server running on PORT: ${port}`);
 })
 
-const users = [];
+// const users = [];
 
-app.get('/users', (req, res) => {
-    console.log('Hello, Welcome to nodeJs API!');
-    console.log('API Health is OKAY!');
-    res.status(200).send(users);
-})
+// app.get('/users', (req, res) => {
+//     console.log('Hello, Welcome to nodeJs API!');
+//     console.log('API Health is OKAY!');
+//     res.status(200).send(users);
+// })
 
-app.post('/users', async (req, res) => {
-    const salt = await bcrypt.genSalt();
-    const bcryptPassword = await bcrypt.hash(req.body.password, salt);
-    const user = { name: req.body.name, password: bcryptPassword };
-    users.push(user);
-    res.status(201).send(users);
-})
+// app.post('/users', async (req, res) => {
+//     const salt = await bcrypt.genSalt();
+//     const bcryptPassword = await bcrypt.hash(req.body.password, salt);
+//     const user = { name: req.body.name, password: bcryptPassword };
+//     users.push(user);
+//     res.status(201).send(users);
+// })
 
-app.post('/users/login', async (req, res) => {
-    const user = users.find(user => user.name === req.body.name)
-    if (user == null) {
-        return res.status(400).send('Cannot find user')
-    }
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            res.send('User Login Success')
-        } else {
-            res.send('User Not Allowed')
-        }
-    } catch {
-        res.status(500).send()
-    }
-})
+// app.post('/users/login', async (req, res) => {
+//     const user = users.find(user => user.name === req.body.name)
+//     if (user == null) {
+//         return res.status(400).send('Cannot find user')
+//     }
+//     try {
+//         if (await bcrypt.compare(req.body.password, user.password)) {
+//             res.send('User Login Success')
+//         } else {
+//             res.send('User Not Allowed')
+//         }
+//     } catch {
+//         res.status(500).send()
+//     }
+// })
