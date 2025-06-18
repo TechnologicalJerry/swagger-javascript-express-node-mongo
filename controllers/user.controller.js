731648a3c1880c7bf80b63afd06ba388d9987174
TechnologicalJerry <cserajatverma@gmail.com>
2025-06-19 01:44:13 +0530

    Create user.controller.js

---
+++ controllers/user.controller.js
@@ -0,0 +1,54 @@
+const User = require("../models/user.model");
+
+// Create new user
+exports.create = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).send(user);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+
+// Get all users
+exports.findAll = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.send(users);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+
+// Get single user by ID
+exports.findOne = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return res.status(404).send({ message: "User not found" });
+        res.send(user);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+
+// Update user by ID
+exports.update = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!user) return res.status(404).send({ message: "User not found" });
+        res.send(user);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+
+// Delete user
+exports.delete = async (req, res) => {
+    try {
+        const user = await User.findByIdAndRemove(req.params.id);
+        if (!user) return res.status(404).send({ message: "User not found" });
+        res.send({ message: "User deleted successfully" });
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+};
