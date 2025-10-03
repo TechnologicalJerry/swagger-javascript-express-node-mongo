c260de0b16ace8b544d121f2f677e385c2436680
TechnologicalJerry <cserajatverma@gmail.com>
2025-10-03 16:11:22 +0530

    Add API routes for auth and users
    Integrated auth and users routers under /api/auth and /api/users endpoints in the main router to modularize API route handling.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var authRouter = require('./auth');
+var usersRouter = require('./users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// API Routes
+router.use('/api/auth', authRouter);
+router.use('/api/users', usersRouter);
 
 module.exports = router;
